b7c9fdce4cb00f5e7b8d9c53a30a10ccbb3801bc
Lanie Williams <lanie_williams@baylor.edu>
2022-02-26 12:37:07 -0600

    adjusting the ships speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,7 +25,7 @@ class AlienInvasion:
         while True:
             #Watch for keyboard and mouse events.
             self._check_events()
-            
+            self.ship.update()
             # Redraw the screen during each pass through the loop.
             self._update_screen()
 
@@ -36,12 +36,23 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         #Update images on the screen, and flip to the new screen
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
