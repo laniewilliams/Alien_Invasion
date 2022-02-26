c438a4a37901f24814683944d9939d38af8f59aa
Lanie Williams <lanie_williams@baylor.edu>
2022-02-26 11:59:41 -0600

    Adding the Ship Image

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -16,6 +17,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
         
 
     def run_game(self):
@@ -28,7 +30,8 @@ class AlienInvasion:
             
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
-
+            self.ship.blitme()
+            
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
