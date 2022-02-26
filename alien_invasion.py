9cce4a8f970940b38456b2e8c5cd09f9fd870a72
Lanie Williams <lanie_williams@baylor.edu>
2022-02-26 11:41:12 -0600

    Starting the Game Project

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -9,17 +11,24 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings() #creating an instance of settings
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             #Watch for keyboard and mouse events.
-            for even in pygame.event.get():
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit
+                    sys.exit()
             
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
