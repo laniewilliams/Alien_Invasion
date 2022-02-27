3f6da91b1a0bac02f25f902e09ee2c19830242dc
Lanie Williams <lanie_williams@baylor.edu>
2022-02-27 14:26:22 -0600

    Ch. 14 adding levels

--- settings.py
+++ settings.py
@@ -8,20 +8,36 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230,230,230)
 
-        # Ship settings
-        self.ship_speed = 1.5
+        # Ship settings 
         self.ship_limit = 2  # setting it to 2 allows there to be 3 chances for the game since the checking if ships left is > 0 happens after it's been hit by an alien
 
         # Bullet settings
-        self.bullet_speed = 1.5
         self.bullet_width = 3.0
         self.bullet_height = 15
         self.bullet_color = (255,0,0)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represetns right; -1 represents left
+
+        #How quickly the game sppeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        #Initialize settings that change throughout the game
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        #fleet direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        #Increase speed settings
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
 
